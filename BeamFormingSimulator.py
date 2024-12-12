5e9da615d348cfb0e9b621734dc2d76895b3c6e4
Habiba Alaa <125210927+habibaalaa123@users.noreply.github.com>
2024-12-12 02:41:53 +0200

    merging

--- BeamFormingSimulator.py
+++ BeamFormingSimulator.py
@@ -1,6 +1,9 @@
 import sys
 import numpy as np
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QSlider, QLabel, QRadioButton, QButtonGroup, QComboBox
+from PyQt5 import uic
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QSlider, QLabel, QRadioButton, \
+    QButtonGroup, QComboBox, QHBoxLayout
 from PyQt5.QtCore import Qt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -8,8 +11,9 @@ from matplotlib.figure import Figure
 class BeamformingVisualizer(QMainWindow):
     def __init__(self):
         super().__init__()
+        uic.loadUi("BeamForming.ui", self)
         self.setWindowTitle("Beamforming Visualizer")
-        self.setGeometry(100, 100, 1200, 800)
+        self.setWindowIcon(QIcon("Deliverables/wifi-signal.png"))
         self.num_antennas = 3
         self.angle_resolution = 1
         self.frequency = 3e8
@@ -23,41 +27,84 @@ class BeamformingVisualizer(QMainWindow):
         self.antenna_layout = "Linear"
 
 
-        central_widget = QWidget()
-        layout = QVBoxLayout()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
+        # central_widget = QWidget()
+        mode_layout = self.findChild(QHBoxLayout, "mode_layout")
+        # central_widget.setLayout(layout)
+        # self.setCentralWidget(central_widget)
 
         # visulization
-        self.figure = Figure()
-        self.canvas = FigureCanvas(self.figure)
-        layout.addWidget(self.canvas)
+        self.beamforming_layout = self.findChild(QVBoxLayout, "beamforming_layout")
+        self.beamprofile_layout = self.findChild(QVBoxLayout, "beamProfile_layout")
+        self.beamprofile_widget = self.findChild(QWidget, "beamforming_map_2")
+        self.beamforming_widget = self.findChild(QWidget, "widget")
+
+        self.beamprofile_layout.removeWidget(self.beamprofile_widget)
+        self.beamforming_layout.removeWidget(self.beamforming_widget)
+        self.beamprofile_widget.deleteLater()
+        self.beamforming_widget.deleteLater()
 
 
-        self.create_mode_selection(layout)
+
+        self.beamprofile_figure = Figure()
+        self.beamforming_figure = Figure()
+        self.beamforming_canvas = FigureCanvas(self.beamforming_figure)
+        self.beamforming_layout.addWidget(self.beamforming_canvas)
+        self.beamprofile_canvas = FigureCanvas(self.beamprofile_figure)
+        self.beamprofile_layout.addWidget(self.beamprofile_canvas)
+
+
+        self.create_mode_selection(mode_layout)
 
         # sliders
-        self.antenna_slider = self.create_num_antennas_slider(layout)
-        self.beam_slider = self.create_beam_direction_slider(layout)
-        self.frequency_slider = self.create_frequency_slider(layout)
-        self.spacing_slider = self.create_spacing_slider(layout)
-        self.curvature_slider = self.create_curvature_slider(layout)
-        self.layout_selector = self.create_layout_selector(layout)
+        self.antenna_slider = self.findChild(QSlider, "Transmitter_slider")
+        self.antenna_label = self.findChild(QLabel, "transmitter_label")
+        self.antenna_slider.setMinimum(1)
+        self.antenna_slider.setMaximum(129)
+        self.antenna_slider.setSingleStep(2)
+        self.antenna_slider.setValue(self.num_antennas)
+        self.antenna_label.setText(str(self.num_antennas))
+        self.antenna_slider.valueChanged.connect(lambda value: self.update_num_antennas(self.antenna_label, value))
+        self.beam_slider = self.findChild(QSlider, "shift_slider")
+        self.beam_label = self.findChild(QLabel, "shift_label")
+        self.beam_slider.setMinimum(-90)
+        self.beam_slider.setMaximum(90)
+        self.beam_slider.setValue(self.beam_direction)
+        self.beam_label.setText(str(self.beam_direction))
+        self.beam_slider.valueChanged.connect(lambda value: self.update_beam_direction(self.beam_label, value))
+
+        self.frequency_slider = self.findChild(QSlider, "frequency_slider")
+        self.frequency_label = self.findChild(QLabel, "frequency_label")
+        self.frequency_unit = self.findChild(QLabel, "frequency_unit")
+        self.frequency_slider.setMinimum(3)  # Representing 3e8 / 10
+        self.frequency_slider.setMaximum(10)  # Representing 4e8 / 10
+        self.frequency_slider.setValue(3)
+        self.frequency_label.setText("0.3")
+        self.frequency_slider.valueChanged.connect(lambda value: self.update_frequency(self.frequency_label, value))
+
+        self.spacing_slider = self.findChild(QSlider, "roc_slider")
+        self.spacing_label = self.findChild(QLabel, "roc_label")
+        self.spacing_type = self.findChild(QLabel, "label_of_roc")
+        scale_factor = 10
+        self.spacing_slider.setMinimum(int(0.5 * scale_factor))
+        self.spacing_slider.setMaximum(int(2.0 * scale_factor))
+        self.spacing_slider.setValue(int((self.spacing / self.wavelength) * scale_factor))
+        self.spacing_label.setText(str((self.spacing / self.wavelength)))
+        self.spacing_slider.valueChanged.connect(lambda value: self.update_spacing(self.spacing_label, value / scale_factor))
+
+        self.layout_selector = self.findChild(QComboBox, "arraygeometry")
+        self.layout_selector.addItems(["Linear", "Curved"])
+        self.layout_selector.currentTextChanged.connect(self.update_layout)
 
         self.update_visualization()
 
     def create_mode_selection(self, layout):
-        label = QLabel("Select Mode:")
-        layout.addWidget(label)
         self.mode_group = QButtonGroup()
-        transmitting_button = QRadioButton("Transmitting")
+        transmitting_button = self.findChild(QRadioButton, "transmitter_radioButton")
         transmitting_button.setChecked(True)
-        receiving_button = QRadioButton("Receiving")
+        receiving_button = self.findChild(QRadioButton, "reciever_radioButton")
         self.mode_group.addButton(transmitting_button)
         self.mode_group.addButton(receiving_button)
 
-        layout.addWidget(transmitting_button)
-        layout.addWidget(receiving_button)
         transmitting_button.toggled.connect(lambda: self.update_mode("Transmitting"))
         receiving_button.toggled.connect(lambda: self.update_mode("Receiving"))
 
@@ -77,90 +124,17 @@ class BeamformingVisualizer(QMainWindow):
         self.beam_slider.setValue(self.beam_direction)
         self.frequency_slider.setValue(3)
         self.spacing_slider.setValue(int((self.spacing / self.wavelength) * 10))
-        self.curvature_slider.setValue(0)
 
-    def create_num_antennas_slider(self, layout):
-        label = QLabel(f"Number of Antennas: {self.num_antennas}")
-        layout.addWidget(label)
 
-        slider = QSlider(Qt.Horizontal)
-        slider.setMinimum(1)
-        slider.setMaximum(129)
-        slider.setSingleStep(2)
-        slider.setValue(self.num_antennas)
-        slider.valueChanged.connect(lambda value: self.update_num_antennas(label, value))
-        layout.addWidget(slider)
-        return slider
-
-    def create_beam_direction_slider(self, layout):
-        label = QLabel(f"Beam Steering Angle (\u00b0): {self.beam_direction}")
-        layout.addWidget(label)
-
-        slider = QSlider(Qt.Horizontal)
-        slider.setMinimum(-90)
-        slider.setMaximum(90)
-        slider.setValue(self.beam_direction)
-        slider.valueChanged.connect(lambda value: self.update_beam_direction(label, value))
-        layout.addWidget(slider)
-        return slider
-
-    def create_frequency_slider(self, layout):
-        label = QLabel(f"Frequency (Hz): {self.frequency:.2e}")
-        layout.addWidget(label)
-
-        slider = QSlider(Qt.Horizontal)
-        slider.setMinimum(3)  # Representing 3e8 / 10
-        slider.setMaximum(10)  # Representing 4e8 / 10
-        slider.setValue(3)
-        slider.valueChanged.connect(lambda value: self.update_frequency(label, value))
-        layout.addWidget(slider)
-        return slider
-
-    def create_spacing_slider(self, layout):
-
-        label = QLabel(f"Antenna Spacing (\u03bb): {self.spacing / self.wavelength:.2f}")
-        layout.addWidget(label)
-
-        scale_factor = 10
-        slider = QSlider(Qt.Horizontal)
-        slider.setMinimum(int(0.5 * scale_factor))
-        slider.setMaximum(int(2.0 * scale_factor))
-        slider.setValue(int((self.spacing / self.wavelength) * scale_factor))
-        slider.valueChanged.connect(lambda value: self.update_spacing(label, value / scale_factor))
-        layout.addWidget(slider)
-        return slider
-
-    def create_curvature_slider(self, layout):
-
-        label = QLabel(f"Curvature: {self.curvature}")
-        layout.addWidget(label)
-
-        slider = QSlider(Qt.Horizontal)
-        slider.setMinimum(0)
-        slider.setMaximum(100)
-        slider.setValue(self.curvature)
-        slider.valueChanged.connect(lambda value: self.update_curvature(label, value))
-        layout.addWidget(slider)
-        return slider
-
-    def create_layout_selector(self, layout):
-        label = QLabel("Antenna Layout:")
-        layout.addWidget(label)
-
-        combo = QComboBox()
-        combo.addItems(["Linear", "Curved"])
-        combo.currentTextChanged.connect(self.update_layout)
-        layout.addWidget(combo)
-        return combo
 
     def update_num_antennas(self, label, value):
         self.num_antennas = value
-        label.setText(f"Number of Antennas: {value}")
+        label.setText(f" {value}")
         self.update_visualization()
 
     def update_beam_direction(self, label, value):
         self.beam_direction = value
-        label.setText(f"Beam Steering Angle (\u00b0): {value}")
+        label.setText(f" {value}")
         self.update_visualization()
 
     def update_frequency(self, label, frequency_value):
@@ -168,23 +142,41 @@ class BeamformingVisualizer(QMainWindow):
         self.frequency = frequency_value * 1e8
         self.wavelength = self.speed_of_light / self.frequency
         self.spacing = self.spacing_slider.value() / 10 * self.wavelength
-        label.setText(f"Frequency (Hz): {self.frequency:.2e}")
+        label.setText(f" {self.frequency / 1e9}")
         self.update_visualization()
 
     def update_spacing(self, label, spacing_ratio):
 
         self.spacing = spacing_ratio * self.wavelength
-        label.setText(f"Antenna Spacing (\u03bb): {spacing_ratio:.2f}")
+        label.setText(f" {spacing_ratio}")
         self.update_visualization()
 
     def update_curvature(self, label, value):
         self.curvature = value
-        label.setText(f"Curvature: {value}")
+        label.setText(f"{value}")
         self.update_visualization()
 
     def update_layout(self, layout_type):
         """Update the layout type."""
         self.antenna_layout = layout_type
+
+        if self.antenna_layout == "Linear":
+            scale_factor = 10
+            self.spacing_type.setText("Distance:")
+            self.spacing_slider.setMinimum(int(0.5 * scale_factor))
+            self.spacing_slider.setMaximum(int(2.0 * scale_factor))
+            self.spacing_slider.setValue(int((self.spacing / self.wavelength) * scale_factor))
+            self.spacing_slider.valueChanged.connect(
+                lambda value: self.update_spacing(self.spacing_label, value / scale_factor))
+            self.spacing_label.setText("0.5")
+        else:
+            self.spacing_type.setText("Radius:")
+
+            self.spacing_slider.setMinimum(0)
+            self.spacing_slider.setMaximum(100)
+            self.spacing_slider.setValue(1)
+            self.spacing_slider.valueChanged.connect(lambda value: self.update_curvature(self.spacing_label, value))
+            self.spacing_label.setText(str(self.curvature))
         self.update_visualization()
 
     def generate_antenna_positions(self, num_antennas, curvature, layout_type='Linear'):
@@ -201,9 +193,10 @@ class BeamformingVisualizer(QMainWindow):
             ])
 
     def update_visualization(self):
-        self.figure.clear()
-        ax1 = self.figure.add_subplot(121, polar=True)
-        ax2 = self.figure.add_subplot(122)
+        self.beamforming_figure.clear()
+        self.beamprofile_figure.clear()
+        ax1 = self.beamprofile_figure.add_subplot(polar=True)
+        ax2 = self.beamforming_figure.add_subplot()
 
         if self.mode == "Transmitting":
             self.plot_beam_profile(ax1)
@@ -212,14 +205,15 @@ class BeamformingVisualizer(QMainWindow):
             self.plot_beam_profile(ax1)
             self.plot_receiving(ax2) # this is the field map
 
-        self.canvas.draw()
+        self.beamforming_canvas.draw()
+        self.beamprofile_canvas.draw()
 
     def plot_beam_profile(self, ax):
         d = self.spacing
-        k = 2 * np.pi / self.wavelength # num of waves
+        k = 2 * np.pi / self.wavelength  # Number of waves
         steering_delay = -k * d * np.sin(np.radians(self.beam_direction))
 
-       # caluculting the array factor
+        # Calculating the array factor
         theta_rad = np.radians(self.theta)
         array_factor = np.abs(np.sum(
             [np.exp(1j * (n - 1) * (k * d * np.sin(theta_rad) + steering_delay)) for n in
@@ -229,29 +223,39 @@ class BeamformingVisualizer(QMainWindow):
 
         array_factor_db = 20 * np.log10(array_factor / np.max(array_factor))
         label = "Array Factor (Transmitting)" if self.mode == "Transmitting" else "Array Factor (Receiving)"
-        ax.plot(np.radians(self.theta), array_factor_db, label=label)
-        ax.set_title("Beam Profile", va='bottom')
-        ax.legend()
+
+        # Set dark theme for the plot
+        ax.set_facecolor("#222222")  # Dark gray background for axes
+        self.beamprofile_figure.patch.set_facecolor("#222222")  # Match figure background
+        ax.tick_params(colors="white")  # White tick labels
+        ax.spines[:].set_color("white")  # White spines
+
+        # Plot the array factor
+        ax.plot(np.radians(self.theta), array_factor_db, label=label, color="red", linewidth=1.5)
+
+
+        # Add legend with adjusted colors
+        legend = ax.legend(loc = 'upper right', facecolor="#333333", edgecolor="white")
+        for text in legend.get_texts():
+            text.set_color("white")
+
 
     def plot_interference_map(self, ax):
+        # Generate grid for interference map
         x = np.linspace(-10, 10, 100)
         y = np.linspace(-10, 10, 100)
         X, Y = np.meshgrid(x, y)
 
-
-        # transmitter_positions = np.array([
-        #     [n * self.spacing, 0] for n in range(-(self.num_antennas - 1) // 2, (self.num_antennas + 1) // 2)
-        # ])
+        # Calculate transmitter positions
         transmitter_positions = self.generate_antenna_positions(self.num_antennas, self.curvature, self.antenna_layout)
 
         k = 2 * np.pi / self.wavelength
         steering_delay = -k * self.spacing * np.sin(np.radians(self.beam_direction))
 
-
         field_map = np.zeros_like(X, dtype=complex)
 
         for idx, pos in enumerate(transmitter_positions):
-            # calculate distances from mesh to trasnmitter
+            # Calculate distances from mesh to transmitter
             distances = np.sqrt((X - pos[0]) ** 2 + (Y - pos[1]) ** 2)
             distances[distances == 0] = 1e-6  # Avoid division by zero
 
@@ -261,15 +265,34 @@ class BeamformingVisualizer(QMainWindow):
         interference_map = np.abs(field_map) ** 2
         interference_map /= np.max(interference_map)  # Normalize
 
-        im = ax.imshow(interference_map, extent=[-10, 10, -10, 10], origin="lower", cmap="hot", aspect="auto")
-        cbar = self.figure.colorbar(im, ax=ax)
-        cbar.set_label("Interference Intensity")
-        ax.set_title("Interference Map with Steering")
-        ax.set_xlabel("x (meters)")
-        ax.set_ylabel("y (meters)")
+        # Set dark theme
+        ax.set_facecolor("#222222")  # Dark gray background for axes
+        self.beamforming_figure.patch.set_facecolor("#222222")  # Match figure background
+        ax.tick_params(colors="white")  # White tick labels
+        ax.spines[:].set_color("white")  # White spines
 
-        ax.scatter(transmitter_positions[:, 0], transmitter_positions[:, 1], c='blue', marker='o', label="Transmitters")
-        ax.legend()
+        # Plot the interference map
+        im = ax.imshow(interference_map, extent=[-10, 10, -10, 10], origin="lower",
+                       cmap="hot", aspect="auto")
+
+        # Add colorbar with adjusted style
+        cbar = self.beamforming_figure.colorbar(im, ax=ax)
+        cbar.ax.yaxis.set_tick_params(color="white")
+        cbar.outline.set_edgecolor("white")
+        cbar.set_label("Interference Intensity", color="white")
+
+        # Set axis labels
+        ax.set_xlabel("x (meters)", color="white")
+        ax.set_ylabel("y (meters)", color="white")
+
+        # Plot transmitter positions
+        ax.scatter(transmitter_positions[:, 0], transmitter_positions[:, 1],
+                   c='cyan', marker='o', label="Transmitters")
+
+        # Add legend with adjusted text color
+        legend = ax.legend(facecolor="#333333", edgecolor="white")
+        for text in legend.get_texts():
+            text.set_color("white")
 
     def plot_receiving(self, ax):
 
@@ -277,6 +300,10 @@ class BeamformingVisualizer(QMainWindow):
         y = np.linspace(-10, 10, 100)
         X, Y = np.meshgrid(x, y)
 
+        ax.set_facecolor("#222222")  # Dark gray background for axes
+        self.beamforming_figure.patch.set_facecolor("#222222")  # Match figure background
+        ax.tick_params(colors="white")  # White tick labels
+        ax.spines[:].set_color("white")  # White spines
         # transmitter fixed at position
         transmitter_position = np.array([0, 10])
 
@@ -299,8 +326,11 @@ class BeamformingVisualizer(QMainWindow):
 
         im = ax.imshow(field_map, extent=[-10, 10, -10, 10], origin="lower", cmap="hot", aspect="auto")
         # color bar
-        cbar = self.figure.colorbar(im, ax=ax)
-        cbar.set_label("Field Intensity")
+        cbar = self.beamforming_figure.colorbar(im, ax=ax)
+        cbar.set_label("Field Intensity", color = "white")
+        cbar.ax.yaxis.set_tick_params(color="white")
+        cbar.outline.set_edgecolor("white")
+
         # vis for transmitter as point
         ax.scatter(transmitter_position[0], transmitter_position[1], color="blue", label="Transmitter", s=100,
                    marker="o")
@@ -308,9 +338,9 @@ class BeamformingVisualizer(QMainWindow):
         for receiver in receiver_positions:
             ax.scatter(receiver[0], receiver[1], color="black",
                        label="Receiver" if receiver is receiver_positions[0] else "", s=100, marker="x")
-        ax.set_title("Receiving Mode Field Map")
-        ax.set_xlabel("x (meters)")
-        ax.set_ylabel("y (meters)")
+        ax.set_xlabel("x (meters)", color="white")
+        ax.set_ylabel("y (meters)", color="white")
+
         ax.legend()
 
 
